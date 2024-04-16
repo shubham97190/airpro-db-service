0a0f726f9b3801502128c29652b117615a13b1f4
Shubham Prajapati <shubhamprajapati.bca2014@gmail.com>
2024-04-17 00:47:45 +0530

    add api

--- base/views.py
+++ base/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import AllowAny
 
@@ -11,3 +13,15 @@ class DeviceModelViewSet(ModelViewSet):
     serializer_class = DeviceModelSerializer
     http_method_names = ['get','post','update','delete']
     permission_classes = [AllowAny]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.data
+        obj = DeviceModel.objects.filter(**data)
+        if obj.exists():            
+            serializer = self.get_serializer(obj[0])
+        else:
+            self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
