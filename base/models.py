8de909bb60fe9c5ebb2f6d39bd1c5447312bcaa1
Shubham Prajapati <shubhamprajapati.bca2014@gmail.com>
2024-04-17 00:24:40 +0530

    add device modal

--- base/models.py
+++ base/models.py
@@ -1,16 +1,20 @@
 import uuid
 from django.db import models
 
-
 # Create your models here.
-class BaseModel(models.Model):
+
+class DeviceModel(models.Model):
     id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     device_id       = models.CharField(max_length=255, blank=True, null=True)  
     serial_num      = models.CharField(max_length=255, blank=True, null=True)  
     mac_addr        = models.CharField(max_length=255, blank=True, null=True)
+
+class BaseModel(models.Model):
+    id              = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    device          = models.ForeignKey(DeviceModel, on_delete=models.CASCADE)
     data            = models.JSONField()
     created_on      = models.DateTimeField(auto_now_add=True)
     updated_on      = models.DateTimeField(auto_now=True)
 
     class Meta:
-        abstract = True
+        abstract=True
